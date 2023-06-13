b004b7591404b73861acf6020127b17877fd7eca
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-06-12 21:01:29 -0400

    added gitignore and package.json

--- index.js
+++ index.js
@@ -26,3 +26,36 @@ inquirer
       message: 'Enter the color for the shape (color keyword or hexadecimal number):',
     },
   ])
+  .then((answers) => {
+    const { text, textColor, shape, shapeColor } = answers;
+
+    let shapeInstance;
+    switch (shape) {
+      case 'Triangle':
+        shapeInstance = new Triangle();
+        break;
+      case 'Circle':
+        shapeInstance = new Circle();
+        break;
+      case 'Square':
+        shapeInstance = new Square();
+        break;
+      default:
+        console.error('Invalid shape');
+        return;
+    }
+    shapeInstance.setColor(shapeColor);
+
+    const svgString = `<svg width="300" height="200">
+      <text x="50%" y="50%" text-anchor="middle" fill="${textColor}">${text}</text>
+      ${shapeInstance.render()}
+    </svg>`;
+
+    fs.writeFile('logo.svg', svgString, (err) => {
+      if (err) {
+        console.error('An error occurred while saving the SVG file:', err);
+        return;
+      }
+      console.log('Generated logo.svg');
+    });
+  });
